5cb356ea213fd77802f2df23ae474f95860827ad
Joshua <mwalejoshua89@gmail.com>
2022-07-02 16:44:54 +0300

    commit tree a5184dcb78eb16a470c42ca1240bf97082850ff6

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-const cats = ["Milo", "Otis", "Garfield"];
-
-function destructivelyAppendCat (name) {
-    cats.push(name)
+const cats = (["Milo", "Otis", "Garfield"]);
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
 }
 
-function destructivelyPrependCat (name) {
-    cats.unshift(name);
+function destructivelyRemoveLastCat(name) {
+    cats.pop("Garfield")
 }
-function destructivelyRemoveLastCat () {
-    cats.pop();
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("Milo")
 }
-
-function destructivelyRemoveFirstCat () {
-    cats.shift();
+function appendCat(name) {
+    [...cats, "Broom"];
 }
-
-function appendCat (name) {
-    return [... cats, name];
+function prependCat(name) {
+    ["Arnold", ...cats];
 }
 
-function prependCat (name) {
-    return [name, ...cats];
+
+function removeLastCat () {
+    return cats.slice(0, cats.length-1)
 }
 
 function removeFirstCat () {
     return cats.slice(1);
-}
-
-function removeLastCat () {
-    return cats.slice(0, cats.length - 1);
 }
